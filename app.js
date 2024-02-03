48029c502ebf34911562374348a7274031838d72
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-02-03 18:07:07 -0300

    Criando função de click

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo da adivinhação';
+const paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
+
+function verificarChute() {
+    console.log("Você clicou");
+}
