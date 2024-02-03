1e521673e8550a6b74ac554344eec3cb3352e1d0
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-02-03 18:25:24 -0300

    Função com retorno

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
 function manipularTextos(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -7,5 +9,9 @@ manipularTextos('h1', 'Jogo da adivinhação');
 manipularTextos('p', 'Escolha um número entre 1 e 10')
 
 function verificarChute() {
-    console.log('Você clicou');
+    console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
